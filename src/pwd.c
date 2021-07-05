e6dd8663d633630ee431405105122b61d4ae5165
Ruthenic <mdrakea3@tutanota.com>
2021-07-04 22:35:28 -0400

    `pwd`: add pwd

---
+++ src/pwd.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <pwd.h>
+
+#include "version.h"
+
+/*
+whoami - prints the working user's username (unless --uid is specified)
+Available arguments:
+	--help: show this help message
+	--version: show the version of the program (WIP)
+	--uid: print the users numeric UID, instead of username
+*/
+
+int main(int argc, char** argv) {
+	char wd[PATH_MAX];
+	getcwd(wd, sizeof(wd));
+	if (argc == 1) {
+		printf(wd);
+	} else {
+		for (int i = 0; i < argc; i++) {
+			char* arg = argv[i];
+			/*printf(arg);
+			printf(" ");*/
+			if (!strcmp(arg, "--help")) {
+				char* help = 
+					"Drake's Epic Coreutils (working title) "
+					DRAKECU_VERSION
+					"\n"
+					"pwd - prints the current working directory\n"
+					"Available arguments:\n"
+					"	--help: show this help message\n"
+					"	--version: show the version of the program";
+				printf("%s\n", help);
+				return 0;
+			} else if (!strcmp(arg, "--version")) {
+				printf(DRAKECU_VERSION);
+				return 0;
+			}
+		}
+	}
+}
