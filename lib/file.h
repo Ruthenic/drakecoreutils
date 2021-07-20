b9c91c3e5fced1b95e2bb5e62b9fd71c56bb271a
Ruthenic <mdrakea3@tutanota.com>
2021-07-19 22:53:05 -0400

    ansi-colour.h/file.h: run `clang-format`.
    whoops, forgot to do this the first time around.
    also uses llvm format.

--- lib/file.h
+++ lib/file.h
@@ -1,20 +1,19 @@
 #include <sys/stat.h>
 
-static _Bool isRegularFile(const char *path)
-{
-    struct stat path_stat;
-    lstat(path, &path_stat);
-    return S_ISREG(path_stat.st_mode);
+static _Bool isRegularFile(const char *path) {
+  struct stat path_stat;
+  lstat(path, &path_stat);
+  return S_ISREG(path_stat.st_mode);
 }
 
 static _Bool isDirectory(const char *path) {
-	struct stat path_stat;
-	lstat(path, &path_stat);
-	return S_ISDIR(path_stat.st_mode);
+  struct stat path_stat;
+  lstat(path, &path_stat);
+  return S_ISDIR(path_stat.st_mode);
 }
 
 static _Bool isSymlink(const char *path) {
-	struct stat path_stat;
-	lstat(path, &path_stat);
-	return S_ISLNK(path_stat.st_mode);
+  struct stat path_stat;
+  lstat(path, &path_stat);
+  return S_ISLNK(path_stat.st_mode);
 }
